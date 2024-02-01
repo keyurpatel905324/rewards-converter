f1c4d0f0c8461f48aeeed1194835fb442c508990
keyurpatel905324 <keyurpatel905324@gmail.com>
2024-02-01 18:25:14 -0500

    I have created a RewardValue class which has two methods: one that accepts a cash value and calculates the miles value using the conversion rate, and another one that accepts a miles value and calculates the cash value using the conversion rate. The RewardValue class provides two methods for retrieving reward values: getCashValue() returns the cash value, and getMilesValue() returns the miles value of the reward.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -3,6 +3,7 @@ public class RewardValue {
     private final double milesValue;
     private static final double CONVERSION_RATE = 0.0035; //Conversion Rate
 
+
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
         this.milesValue = cashValue / CONVERSION_RATE;
