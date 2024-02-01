2bf850baf1fb678ed20155017934df848135e537
keyurpatel905324 <keyurpatel905324@gmail.com>
2024-02-01 18:09:09 -0500

    I have created a RewardValue class which has two methods: one that accepts a cash value and calculates the miles value using the conversion rate, and another one that accepts a miles value and calculates the cash value using the conversion rate. The RewardValue class provides two methods for retrieving reward values: getCashValue() returns the cash value, and getMilesValue() returns the miles value of the reward.

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,29 @@
+public class RewardValue {
+    private final double cashValue;
+    private final double milesValue;
+    private static final double CONVERSION_RATE = 0.0035; //Conversion Rate
+
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / CONVERSION_RATE;
+    }
+
+    public RewardValue(int milesValue){
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * CONVERSION_RATE;
+    }
+
+    public double getCashValue(){
+        return this . cashValue;
+    }
+
+    public double getMilesValue(){
+        return this.milesValue;
+    }
+}
+
+
+
+
+
+
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -5,26 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RewardValueTests {
 
     @Test
-    void create_with_cash_value() {
+    void convert_from_cash_to_miles() {
         double cashValue = 100;
         var rewardValue = new RewardValue(cashValue);
+        double expectedMiles = cashValue / 0.0035; // Conversation rate
         assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
-    void create_with_miles_value() {
+    void convert_from_miles_to_cash() {
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
+        double expectedCash = milesValue * 0.0035;//Conversation rate
         assertEquals(milesValue, rewardValue.getMilesValue());
-    }
-
-    @Test
-    void convert_from_cash_to_miles() {
-        assert false;
-    }
-
-    @Test
-    void convert_from_miles_to_cash() {
-        assert false;
     }
 }
